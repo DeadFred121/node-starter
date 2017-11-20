68817ebc6c3d4c008784b2c0fae202bb75b4b8bb
DeadFred <f.cliff1@hotmail.com>
2017-11-20 12:46:11 +1000

    JS temp challenge

--- src/1-converter.js
+++ src/1-converter.js
@@ -2,21 +2,40 @@ function convertCelsiusToFahrenheit(celsius) {
   return (celsius * 1.8) + 32
 }
 
+function convertFahrenheitToCelsius(fahrenheit) {
+  return (fahrenheit - 32) / 1.8
+}
+
+function formatCelsius(celsius){
+  return (`${Math.round(celsius)} °C`)
+}
+
 let fahrenheit
+
+let celsius
+
+let weeklyTemps = [24, 31, 26, 23, 28, 28, 30]
 
 // Monday
 
 const mondayTemperature = 24
-console.log(`On Monday it is ${mondayTemperature}°C`)
 fahrenheit = convertCelsiusToFahrenheit(mondayTemperature)
-console.log(`The Americans would think of that as ${fahrenheit}°F`)
+console.log(`The Americans would think of Monday's temp as ${fahrenheit}°F`)
+celsius = convertFahrenheitToCelsius(convertCelsiusToFahrenheit(mondayTemperature))
+console.log(`On Monday it is ${formatCelsius(celsius)}`)
+
 
 // Tuesday
 
 const tuesdayTemperature = 31
-console.log(`On Tuesday it is ${tuesdayTemperature}°C`)
 fahrenheit = convertCelsiusToFahrenheit(tuesdayTemperature)
-console.log(`The Americans would think of that as ${fahrenheit}°F`)
+console.log(`The Americans would think of Tuesday's temp as ${fahrenheit}°F`)
+celsius = convertFahrenheitToCelsius(convertCelsiusToFahrenheit(tuesdayTemperature))
+console.log(`On Tuesday it is ${formatCelsius(celsius)}`)
+
+console.log(`This week's temperatures are going to be ${weeklyTemps}`)
+console.log(`and the highest temperature will be: ${Math.max(...weeklyTemps) + '°'}`)
+
 
 /*
 
